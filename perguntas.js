48b6eb6177d27d7f4d1f7fc4fd5ed1a877012dea
Tatiane <0000112143163xsp@al.educacao.sp.gov.br>
2024-11-21 08:06:39 -0300

    Alura 3

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'Quais são as vantagens de usar Python?',
+    'Python é uma linguagem de programação versátil e fácil de aprender.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o rio mais longo do mundo?',
+    'O rio mais longo do mundo é o Nilo.'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma variável em programação?',
+    'Uma variável é um espaço na memória que guarda um valor que pode mudar durante a execução do programa.'
+)
+
+criaCartao(
+    'Língua Inglesa',
+    'Como se diz "obrigado" em inglês?',
+    'Obrigado em inglês é "thank you".'
+)
